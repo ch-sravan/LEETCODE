d540efd96c6c556102da4542894b324a016f2ef6
ch-sravan <110601810+ch-sravan@users.noreply.github.com>
2023-01-07 21:11:18 +0530

    1929. Concatenation of Array.py

---
+++ 1929. Concatenation of Array.py
@@ -0,0 +1,6 @@
+LINK:https://leetcode.com/problems/concatenation-of-array/description/
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        a=nums
+        return a+nums
